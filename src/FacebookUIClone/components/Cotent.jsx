2e36da467b12c82c97c75fc4a01176b839faa0f5
Rong Zhao <rongzhao@NBK-15-047.local>
2020-10-19 09:49:12 +0800

    WIP

---
+++ src/FacebookUIClone/components/Cotent.jsx
@@ -0,0 +1,198 @@
+import React from "react";
+
+const ContentLeft = () => {
+  return (
+    <div className="content__left">
+      <div className="item-container">
+        <div className="item-container__item-wrapper">
+          <img
+            src="https://scontent.fkul14-1.fna.fbcdn.net/v/t1.0-1/p100x100/83799814_10216337728137533_3515890250326999040_o.jpg?_nc_cat=109&_nc_sid=7206a8&_nc_ohc=XcLOFbPQ7ewAX_xN6JU&_nc_ht=scontent.fkul14-1.fna&tp=6&oh=b612288712dc10d3b6a930049ccd7825&oe=5FAEB06B"
+            className="item-container__item-wrapper__avatar"
+            alt="avatar"
+          />
+          <div className="item-container__item-wrapper__name">Rong Zhao</div>
+        </div>
+        <div className="item-container__item-wrapper">
+          <img
+            src="https://static.xx.fbcdn.net/rsrc.php/v3/y9/r/7_gcmlwrelX.png"
+            className="item-container__item-wrapper__avatar"
+            alt="avatar"
+          />
+          <div className="item-container__item-wrapper__name">
+            COVID-19 Information Center
+          </div>
+        </div>
+        <div className="item-container__item-wrapper">
+          <img
+            src="https://static.xx.fbcdn.net/rsrc.php/v3/yj/r/tSXYIzZlfrS.png"
+            className="item-container__item-wrapper__avatar"
+            alt="avatar"
+          />
+          <div className="item-container__item-wrapper__name">Friends</div>
+        </div>
+        <div className="item-container__item-wrapper">
+          <img
+            src="https://static.xx.fbcdn.net/rsrc.php/v3/yj/r/0gH3vbvr8Ee.png"
+            className="item-container__item-wrapper__avatar"
+            alt="avatar"
+          />
+          <div className="item-container__item-wrapper__name">Pages</div>
+        </div>
+        <div className="item-container__item-wrapper">
+          <img
+            src="https://static.xx.fbcdn.net/rsrc.php/v3/yj/r/Im_0d7HFH4n.png"
+            className="item-container__item-wrapper__avatar"
+            alt="avatar"
+          />
+          <div className="item-container__item-wrapper__name">Groups</div>
+        </div>
+      </div>
+      <div className="shortcut-container">
+        <div className="shortcut-container__header">
+          <div className="shortcut-container__header--text-wrapper">
+            Your Shortcut
+          </div>
+          <div className="shortcut-container__header--edit-wrapper">Edit</div>
+        </div>
+        <div className="shortcut-container__item-wrapper">
+          <img
+            src="https://static.xx.fbcdn.net/rsrc.php/v3/yd/r/yWz8NIgyM64.png"
+            className="shortcut-container__item-wrapper__avatar"
+            alt="avatar"
+          />
+          <div className="shortcut-container__item-wrapper__name">
+            ZHAO MAKAN
+          </div>
+        </div>
+        <div className="shortcut-container__item-wrapper">
+          <img
+            src="https://scontent.fkul14-1.fna.fbcdn.net/v/t1.0-0/cp0/c141.0.193.193a/s75x225/62608644_10219294603299384_7667361937348362240_n.jpg?_nc_cat=102&_nc_sid=ca434c&_nc_ohc=Tzf6-KfA4m4AX_-XNF-&_nc_ht=scontent.fkul14-1.fna&_nc_tp=28&oh=b13841ea2bc2d46be4da63104b110d1a&oe=5FB2A266"
+            className="shortcut-container__item-wrapper__avatar"
+            alt="avatar"
+          />
+          <div className="shortcut-container__item-wrapper__name">
+            淘宝败家相公娘子天地
+          </div>
+        </div>
+        <div className="shortcut-container__item-wrapper">
+          <img
+            src="https://scontent.fkul14-1.fna.fbcdn.net/v/t1.0-1/c38.0.148.148a/p148x148/1981893_1424574387801633_3707448773475155476_n.jpg?_nc_cat=108&_nc_sid=dbb9e7&_nc_ohc=FejrStC6bqIAX8ZRwnD&_nc_ht=scontent.fkul14-1.fna&_nc_tp=27&oh=bc4fc72659c8b150dcc3962c833d4ddf&oe=5FB22AE8"
+            className="shortcut-container__item-wrapper__avatar"
+            alt="avatar"
+          />
+          <div className="shortcut-container__item-wrapper__name">
+            The Life. The Pictures.
+          </div>
+        </div>
+        <div className="shortcut-container__item-wrapper">
+          <img
+            src="https://scontent.fkul14-1.fna.fbcdn.net/v/t1.0-0/cp0/s75x225/81247559_465140124439572_2856684762632814592_n.jpg?_nc_cat=106&_nc_sid=ca434c&_nc_ohc=qxFTMz_JdmsAX-gi2Pj&_nc_ht=scontent.fkul14-1.fna&_nc_tp=28&oh=07e693d7b295d35d59117ef622878fbc&oe=5FB038F1"
+            className="shortcut-container__item-wrapper__avatar"
+            alt="avatar"
+          />
+          <div className="shortcut-container__item-wrapper__name">
+            Travel Destination
+          </div>
+        </div>
+        <div className="shortcut-container__item-wrapper">
+          <img
+            src="https://scontent.fkul14-1.fna.fbcdn.net/v/t1.0-0/cp0/c392.0.856.856a/s75x225/117644953_2613381032212161_5936734794809029778_o.jpg?_nc_cat=108&_nc_sid=ca434c&_nc_ohc=6iHnMW8cn0cAX_sFgpy&_nc_ht=scontent.fkul14-1.fna&_nc_tp=28&oh=d36b38fec2f2cc933f2989dbc0edbaab&oe=5FB1040F"
+            className="shortcut-container__item-wrapper__avatar"
+            alt="avatar"
+          />
+          <div className="shortcut-container__item-wrapper__name">
+            Love Is Not Tourism Malaysia
+          </div>
+        </div>
+        <div className="shortcut-container__item-wrapper">
+          <img
+            src="https://scontent.fkul14-1.fna.fbcdn.net/v/t1.0-0/cp0/c91.0.300.300a/s75x225/48362801_2179132782126309_2655609439104532480_n.jpg?_nc_cat=102&_nc_sid=ca434c&_nc_ohc=fXGeMhJ-faEAX-rFjTC&_nc_ht=scontent.fkul14-1.fna&_nc_tp=28&oh=af89ffe4dfc635171b71a2901751577c&oe=5FB0C8D1"
+            className="shortcut-container__item-wrapper__avatar"
+            alt="avatar"
+          />
+          <div className="shortcut-container__item-wrapper__name">
+            房地产房屋买卖 Property Sale & Rent
+          </div>
+        </div>
+        <div className="shortcut-container__item-wrapper">
+          <img
+            src="https://scontent.fkul14-1.fna.fbcdn.net/v/t1.0-0/cp0/c245.0.315.315a/s75x225/16996257_10211240675507122_1573384716361964321_n.jpg?_nc_cat=111&_nc_sid=ca434c&_nc_ohc=XKLkxOIIBUcAX9uF92k&_nc_ht=scontent.fkul14-1.fna&_nc_tp=28&oh=8bf16955c151c2d6aaa39b1a09fbfc05&oe=5FB0E7AC"
+            className="shortcut-container__item-wrapper__avatar"
+            alt="avatar"
+          />
+          <div className="shortcut-container__item-wrapper__name">
+            ReactJS Expert Community
+          </div>
+        </div>
+        <div className="shortcut-container__item-wrapper">
+          <img
+            src="https://scontent.fkul14-1.fna.fbcdn.net/v/t1.0-0/cp0/c210.0.540.540a/s75x225/118141980_10158452055070890_4712062015928078173_n.jpg?_nc_cat=101&_nc_sid=ca434c&_nc_ohc=I4sO5balqo4AX_HA7RC&_nc_ht=scontent.fkul14-1.fna&_nc_tp=28&oh=66cc3de6417630d195cb08385bc42a26&oe=5FB00625"
+            className="shortcut-container__item-wrapper__avatar"
+            alt="avatar"
+          />
+          <div className="shortcut-container__item-wrapper__name">
+            Bangkok and Thailand Travellers
+          </div>
+        </div>
+        <div className="shortcut-container__item-wrapper">
+          <img
+            src="https://static.xx.fbcdn.net/rsrc.php/v3/yd/r/yWz8NIgyM64.png"
+            className="shortcut-container__item-wrapper__avatar"
+            alt="avatar"
+          />
+          <div className="shortcut-container__item-wrapper__name">
+            ZHAO MAKAN
+          </div>
+        </div>
+        <div className="shortcut-container__item-wrapper">
+          <img
+            src="https://scontent.fkul14-1.fna.fbcdn.net/v/t1.0-0/cp0/c141.0.193.193a/s75x225/62608644_10219294603299384_7667361937348362240_n.jpg?_nc_cat=102&_nc_sid=ca434c&_nc_ohc=Tzf6-KfA4m4AX_-XNF-&_nc_ht=scontent.fkul14-1.fna&_nc_tp=28&oh=b13841ea2bc2d46be4da63104b110d1a&oe=5FB2A266"
+            className="shortcut-container__item-wrapper__avatar"
+            alt="avatar"
+          />
+          <div className="shortcut-container__item-wrapper__name">
+            淘宝败家相公娘子天地
+          </div>
+        </div>
+        <div className="shortcut-container__item-wrapper">
+          <img
+            src="https://scontent.fkul14-1.fna.fbcdn.net/v/t1.0-1/c38.0.148.148a/p148x148/1981893_1424574387801633_3707448773475155476_n.jpg?_nc_cat=108&_nc_sid=dbb9e7&_nc_ohc=FejrStC6bqIAX8ZRwnD&_nc_ht=scontent.fkul14-1.fna&_nc_tp=27&oh=bc4fc72659c8b150dcc3962c833d4ddf&oe=5FB22AE8"
+            className="shortcut-container__item-wrapper__avatar"
+            alt="avatar"
+          />
+          <div className="shortcut-container__item-wrapper__name">
+            The Life. The Pictures.
+          </div>
+        </div>
+        <div className="shortcut-container__item-wrapper">
+          <img
+            src="https://scontent.fkul14-1.fna.fbcdn.net/v/t1.0-0/cp0/s75x225/81247559_465140124439572_2856684762632814592_n.jpg?_nc_cat=106&_nc_sid=ca434c&_nc_ohc=qxFTMz_JdmsAX-gi2Pj&_nc_ht=scontent.fkul14-1.fna&_nc_tp=28&oh=07e693d7b295d35d59117ef622878fbc&oe=5FB038F1"
+            className="shortcut-container__item-wrapper__avatar"
+            alt="avatar"
+          />
+          <div className="shortcut-container__item-wrapper__name">
+            Travel Destination
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ContentCenter = () => {
+  return <div className="content__center">CENTER</div>;
+};
+
+const ContentRight = () => {
+  return <div className="content__right">RIGHT</div>;
+};
+
+export default function Cotent() {
+  return (
+    <div className="content">
+      <ContentLeft />
+      <ContentCenter />
+      <ContentRight />
+    </div>
+  );
+}
